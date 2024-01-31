baec047eab7fe70039b4f4e2724c1262f34bc0f4
Bipul Hajong <126290681+imBipulH@users.noreply.github.com>
2024-01-31 17:58:14 +0600

    Update firebaseConfig2.jsx

--- src/authentication/firebaseConfig2.jsx
+++ src/authentication/firebaseConfig2.jsx
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig2);
+const app = initializeApp(firebaseConfig);
 // eslint-disable-next-line no-unused-vars
 const analytics = getAnalytics(app);
 
