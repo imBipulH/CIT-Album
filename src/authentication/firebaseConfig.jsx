1b219ef011f8c02bf6ec8e9cfabc9814b9afc117
imBipulH <bipulh62@gmail.com>
2024-01-31 10:11:44 +0600

    2nd commit

---
+++ src/authentication/firebaseConfig.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBsqAHZXItUc0R_0nS2oV0QyHe0Mo3StG4",
+  authDomain: "citalbum-e84fc.firebaseapp.com",
+  projectId: "citalbum-e84fc",
+  storageBucket: "citalbum-e84fc.appspot.com",
+  messagingSenderId: "74578725175",
+  appId: "1:74578725175:web:57b8df0b4fb1ebeada32b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig;
---
+++ src/authentication/firebaseConfig2.jsx
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig2 = {
+  apiKey: "AIzaSyA8izsiqTnaw9qWUQnxs8Rg8u0aoi4rbrQ",
+  authDomain: "album-e1d22.firebaseapp.com",
+  projectId: "album-e1d22",
+  storageBucket: "album-e1d22.appspot.com",
+  messagingSenderId: "641436239903",
+  appId: "1:641436239903:web:894960fd3ea2fa30f72d2a",
+  measurementId: "G-22L5D0MR6E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig2);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+
+export default firebaseConfig2;
