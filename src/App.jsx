fc9c7791ce5dcb5b60cab432472585b70f611196
imBipulH <bipulh62@gmail.com>
2024-02-01 01:09:32 +0600

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState, useRef } from 'react'
 import {
   getStorage,
@@ -14,7 +13,7 @@ function App () {
   const [files, setFiles] = useState([])
   const fileInputRef = useRef(null)
   const [progress, setProgress] = useState()
-  // console.log(file.name)
+  const [name, setName] = useState()
 
   useEffect(() => {
     const fetchImages = async () => {
@@ -24,7 +23,12 @@ function App () {
         const urls = []
         for (const item of imageList.items) {
           const url = await getDownloadURL(item)
-          urls.push({url, ref: item})
+          if (item.name.includes('|')) {
+            const username = item.name.split('|')[0]          
+            urls.push({ url, username, ref: item })
+          } else {
+            urls.push({ url, ref: item })
+          }
           await new Promise(resolve => setTimeout(resolve, 100))
         }
         setFiles(urls)
@@ -33,21 +37,22 @@ function App () {
       }
     }
     fetchImages()
-  })
+  }, [])
 
   const storage = getStorage()
   const metadata = { contentType: 'image/jpeg' }
 
   const handleUpload = () => {
-    const storageRef = ref(storage, 'images/' + `${file ? file.name : file}`)
+    const storageRef = ref(
+      storage,
+      'images/' + `${name}|${file ? file.name : file}`
+    )
     const uploadTask = uploadBytesResumable(storageRef, file, metadata)
-
     uploadTask.on(
       'state_changed',
       snapshot => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        const roundedprogress = Math.round(progress*10)/10
+        const roundedprogress = Math.round(progress * 10) / 10
         setProgress(roundedprogress)
         console.log('Upload is ' + progress + '% done')
         switch (snapshot.state) {
@@ -62,13 +67,13 @@ function App () {
       error => {
         switch (error.code) {
           case 'storage/unauthorized':
-          console.log("User doesn't have permission to access the object") 
+            console.log("User doesn't have permission to access the object")
             break
           case 'storage/canceled':
-            console.log("User canceled the upload") 
+            console.log('User canceled the upload')
             break
           case 'storage/unknown':
-           console.log("Unknown error occurred, inspect error.serverResponse")
+            console.log('Unknown error occurred, inspect error.serverResponse')
             break
         }
       },
@@ -83,7 +88,7 @@ function App () {
     setFile(null)
   }
 
-  const handleDelete = async (item) => {
+  const handleDelete = async item => {
     console.log(item)
     try {
       await deleteObject(item.ref) // Delete the object from storage
@@ -96,37 +101,64 @@ function App () {
   return (
     <>
       <div className='bg-aqua p-6 container mx-auto h-screen'>
-        <h1 className='text-center text-3xl text-teal'>CIT Album</h1>
-          <p className='text-center mt-6 mb-4'>{progress ? `Upload is ${progress}% done.`: `Click "Choose File" and select an image.`}</p>
-
-        <div className='flex justify-center mb-6'>
-          <input
-            ref={fileInputRef}
-            onChange={e => setFile(e.target.files[0])}
-            type='file'
-            className=''
-          />
-          <button
-            onClick={handleUpload}
-            className='bg-white px-2 py-1 rounded-md'
-          >
-            Upload
-          </button>
+        <h1 className='text-center text-3xl text-teal'>CIT Album<span className='text-sm'>1.0</span></h1>
+        <p className='text-center mt-6 mb-4'>
+          {progress
+            ? `Upload is ${progress}% done.`
+            : `Click "Choose File" and select an image.`}
+        </p>
+        <div className='flex gap-4 flex-wrap justify-center mb-6'>
+          <div className='flex gap-2 items-center'>
+            <p className=''>Uploaded by:</p>
+            <input
+              onChange={e => setName(e.target.value)}
+              type='text'
+              placeholder='Name (required)'
+              className='p-1 text-sm outline-none'
+            />
+          </div>
+          <div>
+            <input
+              ref={fileInputRef}
+              onChange={e => setFile(e.target.files[0])}
+              type='file'
+              className='text-sm'
+            />
+            <button
+              onClick={file && name ? handleUpload: null}
+              className={`bg-white px-2 py-1 text-sm rounded-sm ${!name || !file ? 'cursor-not-allowed':''}`}
+            >
+              Upload
+            </button>
+          </div>
         </div>
 
         <div className='flex gap-2 flex-wrap'>
           {files &&
             files.map((url, index) => (
-              <div className='group h-full w-40 border relative border-white p-1' key={index}>
-                 <p onClick={()=> handleDelete(url)} className='absolute right-0 text-white text-sm bg-red-500 leading-3 px-2 py-1 rounded-sm cursor-pointer group-hover:block hidden'>x</p>
-                <img src={url.url} className='max-w-40 h-full items-baseline group' />
+              <div
+                className='group h-full w-40 border relative border-white p-1'
+                key={index}
+              >
+                <p
+                  onClick={() => handleDelete(url)}
+                  className='absolute right-0 text-white text-sm bg-red-500 leading-3 px-2 py-1 rounded-sm cursor-pointer group-hover:block hidden'
+                >
+                  x
+                </p>
+                <img
+                  src={url.url}
+                  className='max-w-40 h-full items-baseline group'
+                />
+                <p className='text-sm text-teal-900'>{url.username && url.username}</p>
               </div>
             ))}
         </div>
         <div className='flex justify-center'>
-         
           <p className='fixed bottom-4 text-xl text-center text-teal-900'>
-           This is a simple web app to upload your files to a web server from your smartdevice. This has been created for learning purpose. Thank you! 
+            This is a simple web app to upload your files to a web server from
+            your smartdevice. This has been created for learning purpose. Thank
+            you!
           </p>
         </div>
       </div>
